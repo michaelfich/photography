ba89eb76ecf3abbb237edfb6ee9de2d56686a86f
Michael Fich <michael@michaelfich.com>
2017-03-22 21:50:43 -0400

    Test photo model is valid without params
    - write test for user model without params
    - use TDD to refactor the model to allow more leniency in constructor params
    - generate photo factory
    - remove helper specs

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -52,83 +52,85 @@ class Photo
     :licensing_suggested,
     :is_free_photo
 
-  def initialize(params)
-    @id = params['id']
-    @user_id = params['user_id']
-    @name = params['name']
-    @description = params['description']
-    @camera = params['camera']
-    @lens = params['lens']
-    @focal_strength = params['focal_strength']
-    @iso = params['iso']
-    @shutter_speed = params['shutter_speed']
-    @aperature = params['aperature']
-    @times_viewed = params['times_viewed']
-    @rating = params['rating']
-    @rating = params['rating']
-    @status = params['status']
-    @created_at = params['created_at']
-    @category = params['category']
-    @category = params['category']
-    @location = params['location']
-    @longitude = params['longitude']
-    @taken_at = params['taken_at']
-    @hi_res_uploaded = params['hi_res_uploaded']
-    @for_sale = params['for_sale']
-    @width = params['width']
-    @height = params['height']
-    @votes_count = params['votes_count']
-    @favorites_count = params['favorites_count']
-    @comments_count = params['comments_count']
-    @nsfw = params['nsfw']
-    @sales_count = params['sales_count']
-    @for_sale_date = params['for_sale_date']
-    @highest_rating = params['highest_rating']
-    @highest_rating_date = params['highest_rating_date']
-    @license_type = params['license_type']
-    @converted = params['converted']
-    @collections_count = params['collections_count']
-    @crop_version = params['crop_version']
-    @privacy = params['privacy']
-    @profile = params['profile']
-    @image_url = params['image_url']
-    @images = params['images'].map do
-      {
-        size: params['size'],
-        url: params['url'],
-        https_url: params['https_url'],
-        format: params['format']
-      }
+  def initialize(params = {})
+    @id = params.dig('id')
+    @user_id = params.dig('user_id')
+    @name = params.dig('name')
+    @description = params.dig('description')
+    @camera = params.dig('camera')
+    @lens = params.dig('lens')
+    @focal_strength = params.dig('focal_strength')
+    @iso = params.dig('iso')
+    @shutter_speed = params.dig('shutter_speed')
+    @aperature = params.dig('aperature')
+    @times_viewed = params.dig('times_viewed')
+    @rating = params.dig('rating')
+    @rating = params.dig('rating')
+    @status = params.dig('status')
+    @created_at = params.dig('created_at')
+    @category = params.dig('category')
+    @category = params.dig('category')
+    @location = params.dig('location')
+    @longitude = params.dig('longitude')
+    @taken_at = params.dig('taken_at')
+    @hi_res_uploaded = params.dig('hi_res_uploaded')
+    @for_sale = params.dig('for_sale')
+    @width = params.dig('width')
+    @height = params.dig('height')
+    @votes_count = params.dig('votes_count')
+    @favorites_count = params.dig('favorites_count')
+    @comments_count = params.dig('comments_count')
+    @nsfw = params.dig('nsfw')
+    @sales_count = params.dig('sales_count')
+    @for_sale_date = params.dig('for_sale_date')
+    @highest_rating = params.dig('highest_rating')
+    @highest_rating_date = params.dig('highest_rating_date')
+    @license_type = params.dig('license_type')
+    @converted = params.dig('converted')
+    @collections_count = params.dig('collections_count')
+    @crop_version = params.dig('crop_version')
+    @privacy = params.dig('privacy')
+    @profile = params.dig('profile')
+    @image_url = params.dig('image_url')
+    if params.dig('images').respond_to? :each
+      @images = params.dig('images').map do
+        {
+          size: params.dig('size'),
+          url: params.dig('url'),
+          https_url: params.dig('https_url'),
+          format: params.dig('format')
+        }
+      end
     end
-    @url = params['url']
-    @positive_votes_count = params['positive_votes_count']
-    @converted_bits = params['converted_bits']
-    @watermark = params['watermark']
-    @image_format = params['image_format']
+    @url = params.dig('url')
+    @positive_votes_count = params.dig('positive_votes_count')
+    @converted_bits = params.dig('converted_bits')
+    @watermark = params.dig('watermark')
+    @image_format = params.dig('image_format')
     @user = {
-      id: params['user']['id'],
-      username: params['user']['username'],
-      firstname: params['user']['firstname'],
-      lastname: params['user']['lastname'],
-      city: params['user']['city'],
-      country: params['user']['country'],
-      usertype: params['user']['usertype'],
-      fullname: params['user']['fullname'],
-      userpic_url: params['user']['userpic_url'],
-      userpic_https_url: params['user']['userpic_https_url'],
-      cover_url: params['user']['cover_url'],
-      upgrade_status: params['user']['upgrade_status'],
-      store_on: params['user']['store_on'],
-      affection: params['user']['affection'],
+      id: params.dig('user', 'id'),
+      username: params.dig('user', 'username'),
+      firstname: params.dig('user', 'firstname'),
+      lastname: params.dig('user, lastname'),
+      city: params.dig('user, city'),
+      country: params.dig('user', 'country'),
+      usertype: params.dig('user', 'usertype'),
+      fullname: params.dig('user', 'fullname'),
+      userpic_url: params.dig('user', 'userpic_url'),
+      userpic_https_url: params.dig('user', 'userpic_https_url'),
+      cover_url: params.dig('user', 'cover_url'),
+      upgrade_status: params.dig('user', 'upgrade_status'),
+      store_on: params.dig('user', 'store_on'),
+      affection: params.dig('user', 'affection'),
       avatars: {
-        default: params['user']['avatars']['default']['https'],
-        large: params['user']['avatars']['large']['https'],
-        small: params['user']['avatars']['small']['https'],
-        tiny: params['user']['avatars']['tiny']['https']
+        default: params.dig('user', 'avatars', 'default', 'https'),
+        large: params.dig('user', 'avatars', 'large', 'https'),
+        small: params.dig('user', 'avatars', 'small', 'https'),
+        tiny: params.dig('user', 'avatars', 'tiny', 'https')
       }
     }
-    @licensing_requested = params['licensing_requested']
-    @licensing_suggested = params['licensing_suggested']
-    @is_free_phot = params['is_free_phot']
+    @licensing_requested = params.dig('licensing_requested')
+    @licensing_suggested = params.dig('licensing_suggested')
+    @is_free_phot = params.dig('is_free_phot')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'initialized without complete parameters' do
+    it 'should not throw any errors' do
+      expect { Photo.new }.to_not raise_error
+    end
+  end
 end
